0f0b6de989376967bf479667f41c5104e2bff7c8
RaditzzZ <raditagit04@gmail.com>
2026-09-14 22:13:43 +0800

    feat: search in navbar

--- src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/LandingPageLayoutNavbar.tsx
+++ src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/LandingPageLayoutNavbar.tsx
@@ -15,6 +15,9 @@ import {
   NavbarMenuItem,
   NavbarMenuToggle,
   Link,
+  Listbox,
+  ListboxItem,
+  Spinner,
 } from "@heroui/react";
 import Image from "next/image";
 import { BUTTON_ITEMS, NAV_ITEMS } from "../LandingPageLayout.constants";
@@ -24,11 +27,20 @@ import { CiSearch } from "react-icons/ci";
 import { signOut, useSession } from "next-auth/react";
 import useLandingPageLayoutNavbar from "./useLandingPageLayoutNavbar";
 import { Fragment } from "react";
+import { IEvent } from "@/types/Event";
 
 const LandingPageLayoutNavbar = () => {
   const router = useRouter();
   const session = useSession();
-  const { dataProfile } = useLandingPageLayoutNavbar();
+  const {
+    dataProfile,
+    dataEventsSearch,
+    isLoadingEventsSearch,
+    isRefecthingEventSearch,
+    handleSearch,
+    search,
+    setSearch,
+  } = useLandingPageLayoutNavbar();
 
   return (
     <Navbar maxWidth="full" isBordered isBlurred={false} shouldHideOnScroll>
@@ -66,9 +78,38 @@ const LandingPageLayoutNavbar = () => {
             placeholder="Search Event"
             className="w-75"
             startContent={<CiSearch />}
-            onClear={() => {}}
-            onChange={() => {}}
+            onClear={() => setSearch("")}
+            onChange={handleSearch}
           />
+          {search !== "" && (
+            <Listbox
+              items={dataEventsSearch?.data || []}
+              className="absolute top-12 right-0 rounded-xl border bg-white"
+            >
+              {!isRefecthingEventSearch && !isLoadingEventsSearch ? (
+                (item: IEvent) => (
+                  <ListboxItem key={item._id} href={`/event/${item.slug}`}>
+                    <div className="flex items-center gap-2">
+                      <Image
+                        src={`${item.banner}`}
+                        alt={`${item.name}`}
+                        className="w-2/5 rounded-md"
+                        width={100}
+                        height={40}
+                      />
+                      <p className="line-clamp-2 w-3/5 text-wrap">
+                        {item.name}
+                      </p>
+                    </div>
+                  </ListboxItem>
+                )
+              ) : (
+                <ListboxItem key="loading">
+                  <Spinner color="danger" size="sm" />
+                </ListboxItem>
+              )}
+            </Listbox>
+          )}
         </NavbarItem>
         {session.status === "authenticated" ? (
           <NavbarItem className="hidden lg:block">
--- src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/useLandingPageLayoutNavbar.tsx
+++ src/components/layouts/LandingPageLayout/LandingPageLayoutNavbar/useLandingPageLayoutNavbar.tsx
@@ -1,10 +1,20 @@
+import {
+  DELAY,
+  LIMIT_BANNER,
+  PAGE_DEFAULT,
+} from "@/components/constants/list.constants";
+import useDebounce from "@/hooks/useDebounce";
 import authServices from "@/services/auth.service";
+import eventService from "@/services/event.service";
 import { useQuery } from "@tanstack/react-query";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/router";
+import { ChangeEvent, useState } from "react";
 
 const useLandingPageLayoutNavbar = () => {
   const router = useRouter();
+  const [search, setSearch] = useState("");
+  const debounce = useDebounce();
   const session = useSession();
 
   const getProfile = async () => {
@@ -18,8 +28,36 @@ const useLandingPageLayoutNavbar = () => {
     enabled: router.isReady && session.status === "authenticated",
   });
 
+  const getEventsSearch = async () => {
+    const params = `search=${search}&limit=${LIMIT_BANNER}&page=${PAGE_DEFAULT}&isPublished=true`;
+    const res = await eventService.getEvents(params);
+    const { data } = res;
+    return data;
+  };
+
+  const {
+    data: dataEventsSearch,
+    isLoading: isLoadingEventsSearch,
+    isRefetching: isRefecthingEventSearch,
+  } = useQuery({
+    queryKey: ["EventsSearch"],
+    queryFn: getEventsSearch,
+    enabled: !!search,
+  });
+
+  const handleSearch = (e: ChangeEvent<HTMLInputElement>) => {
+    debounce(() => setSearch(e.target.value), DELAY);
+  };
+
   return {
     dataProfile,
+
+    dataEventsSearch,
+    isLoadingEventsSearch,
+    isRefecthingEventSearch,
+    handleSearch,
+    search,
+    setSearch,
   };
 };
 
